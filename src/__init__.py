1db321ef03742f72942ae9965c3c93dfcd8a2f4c
Technik Tüftler <technik.tueftler@gmail.com>
2024-11-24 15:20:22 +0100

    revision of the descriptions

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,9 @@
-"""Src
+"""
+This module initializes the main application and provides central functions, 
+classes and configurations. It serves as the entry point for the entire application 
+and enables
+- The loading of global configurations and resources.
+- The initialization of submodules and packages.
+- The import and provision of frequently used functions and constants.
 """
 from .tetue_generic.generic_requests import *
--- src/tetue_generic/__init__.py
+++ src/tetue_generic/__init__.py
@@ -1,5 +1,14 @@
-"""Generic init
 """
+This module initializes the package and provides central variables and configurations.
+- Defines generic standard variables that can be used throughout the package.
+- Imports variables from the parent module if they are already defined to avoid redundancy.
+- Enables centralized management and reusability of shared resources.
+
+Note:
+- Parent variables are only imported if they are explicitly defined in the parent module.
+- Variables from higher-level take precedence.
+"""
+
 REQUEST_TIMEOUT = 20
 LOG_FILE_PATH = "files/henCommander.log"
 try:
