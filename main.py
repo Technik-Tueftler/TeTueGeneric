e2d7e6fe768b4b63c92d20f82deebf3479530f5a
Technik Tüftler <technik.tueftler@gmail.com>
2025-03-24 07:56:47 +0100

    cleanup

--- main.py
+++ main.py
@@ -10,12 +10,8 @@ def main() -> None:
     Scheduling function for regular call.
     """
     config = src.Configuration()
-    print(type(config))
-    print(type(config.watcher))
-    src.watcher.init_logging(config.watcher)
-    src.watcher.logger.info(f"Start application in version: {src.__version__}")
-    print(config.gen_req.request_timeout)
-    print(config.watcher.log_level)
+    src.init_logging(config.watcher)
+    src.logger.info(f"Start application in version: {src.__version__}")
 
 
 if __name__ == "__main__":
